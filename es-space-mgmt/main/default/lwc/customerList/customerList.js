138d8a85df254519d7d982073156dcdcd2780c12
Siva Krishna <nanibhanu143@gmail.com>
2020-08-31 20:47:13 -0400

    Commit until message channels

--- es-space-mgmt/main/default/lwc/customerList/customerList.js
+++ es-space-mgmt/main/default/lwc/customerList/customerList.js
@@ -1,6 +1,15 @@
 import { LightningElement, api, wire } from 'lwc';
 import getCustomerList from '@salesforce/apex/reservationManagerController.getCustomerList';
 
+import TILE_SELECTION_MC from '@salesforce/messageChannel/Tile_Selection__c';
+import {
+    subscribe,
+    unsubscribe,
+    APPLICATION_SCOPE,
+    MessageContext,
+    publish
+} from 'lightning/messageService';
+
 export default class CustomerList extends LightningElement {
     @api sobject = 'Lead';
     customers = [];
@@ -8,6 +17,9 @@ export default class CustomerList extends LightningElement {
     errorMsg;
     msgForUser;
     wiredRecords;
+
+    @wire(MessageContext)
+    messageContext;
 
     @wire(getCustomerList, { sObjectType: '$sobject' })
     wiredCustomers(value) {
@@ -22,7 +34,7 @@ export default class CustomerList extends LightningElement {
     }
 
     publishSelect(event) {
-        console.log(JSON.stringify(event.detail));
+        const payload = { tileType: 'customer', properties: event.detail };
+        publish(this.messageContext, TILE_SELECTION_MC, payload);
     }
-
 }
